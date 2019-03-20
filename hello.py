da4ec8f4a8bfda48039eb5ec4f0b5dd79602adf9
jenyykim <48500722+jenyykim@users.noreply.github.com>
2019-03-20 10:39:54 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
+// For command line git tutorial
+// Github modification
+
 print("hello world")
 print("tell your world)
 print("tell his world")
 print("tell her world")
+print("tell boys world")
+print("tell girls world")
